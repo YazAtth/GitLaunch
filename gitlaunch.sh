16f897ec0ed3e490c82f60126ab0287403c61b96
YazAtth <yasith.athan@gmail.com>
2023-09-13 18:04:30 +0100

    FEAT: Incomplete implementation of stopping program when error is encountered

--- gitlaunch.sh
+++ gitlaunch.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+function start_spinner {
+    set +m
+    tput sc  # Save the cursor position
+    echo -en "$GREEN_BOLD* $1$WHITE     "
+
+    { while : ; do for X in ⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷ ; do echo -en "\b$BLUE$X$WHITE" ; sleep 0.1 ; done ; done & } 2>/dev/null
+
+    spinner_pid=$!
+}
+
+function stop_spinner {
+    { kill -9 $spinner_pid && wait; } 2>/dev/null
+    set -m
+    echo -en "\033[2K\r"
+}
+
+function print_message {
+
+    logging_text=$(cat)
+
+    tput rc  # Restore the cursor position
+    echo -e "\033[K$logging_text"  # Erase to the end of the line and print the new message
+    tput sc  # Save the cursor position again
+
+
+    echo -en "$GREEN_BOLD* $1$WHITE     "
+}
+
+
 repoName=${1? missing name of repo name}
 
 # Shift the processed options and their arguments
@@ -9,6 +38,8 @@ isRepoPublic=false
 
 GREEN_BOLD='\033[1;32m'
 WHITE='\033[0;37m'
+BLUE='\033[1;36m'
+
 
 
 if ! [ -f README.md ]; then
@@ -22,14 +53,13 @@ if ! [ -d .git ]; then
   echo -e $GREEN_BOLD
   echo "* Git not intialised in current directory"
   echo "* Initialising current repository with git..."
+
   echo -e $WHITE
 
-  
+  git init >/dev/null
+  git add . >/dev/null
+  git commit -m "FEAT: Setup (first commit)" >/dev/null
 
-
-  git init
-  git add .
-  git commit -m "FEAT: Setup (first commit)"
 fi
 
 
@@ -50,12 +80,31 @@ shift $((OPTIND-1))
 
 echo ""
 
+
+
 if ! $isRepoPublic; then
-  echo -e "$GREEN_BOLD* Creating private GitHub repo.. $WHITE"
-  gh repo create $repoName --private --source=$PWD --remote=upstream --push
+  # echo -e "$GREEN_BOLD* Creating private GitHub repo.. $WHITE"
+  # gh repo create $repoName --private --source=$PWD --remote=upstream --push
+
+  loading_message="Creating private GitHub repo..." 
+
+  spinner_pid=
+  start_spinner "Creating private GitHub repo..."
+
+
+  exec 3>&1
+  number=$(gh repo create $repoName --private --source=$PWD --remote=upstream --push 2>&1 >/dev/null)
+  exec 1>&3
+
+  stop_spinner
+
+  echo "Hello world" 
+  echo -e "Number is $number"
+
+
 else
   echo -e "$GREEN_BOLD* Creating public Github repo... $WHITE"
-  gh repo create $repoName --public --source=$PWD --remote=upstream --push
+  # gh repo create $repoName --public --source=$PWD --remote=upstream --push
 fi
 
 echo -e "$GREEN_BOLD* Created repo on Github.com$WHITE"
@@ -63,3 +112,4 @@ echo -e "$GREEN_BOLD* Created repo on Github.com$WHITE"
 
 
 
+echo "END"
--- test.sh
+++ test.sh
@@ -14,6 +14,9 @@ start_spinner "I'm thinking "
 random_text | print_message "I'm thinking "
 sleep 2
 
+random_text | print_message "I'm thinking "
+sleep 5
+
 stop_spinner
 
 
