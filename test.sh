703316c580c77126d18c82846c9cb9fab87d152f
YazAtth <yasith.athan@gmail.com>
2023-09-14 19:39:21 +0100

    DOCS: Updated README

--- test.sh
+++ test.sh
@@ -4,4 +4,14 @@
 git_msg="Your branch is up to date with 'upstream/dev'."
 
 git status | grep -wq "$git_msg"
-echo $?
+is_pending_commits=$?
+
+if [ $is_pending_commits -eq 1 ]; then
+  echo "Pending commits"
+else
+  echo "No pending commits"
+fi
+
+
+# 1 = pending commits
+# 0 = no pending commuts
